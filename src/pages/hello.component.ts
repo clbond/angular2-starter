cb51666f412a1cc35850768c78f06d3dac250729
Chris Bond <chris.bond@rangle.io>
2016-09-23 11:54:28 -0400

    Add hello timer

--- src/pages/hello.component.ts
+++ src/pages/hello.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {Subject} from 'rxjs';
 
 @Component({
   template: `
-    <hello-child></hello-child>
+    <hello-child [counter]="counter"></hello-child>
   `
 })
-export class RioHelloPageComponent {}
+export class RioHelloPageComponent {
+  private counter = new Subject<number>();
+
+  private timer;
+
+  private counterIncrement = 0;
+
+  private ngOnInit() {
+    this.timer = setInterval(
+      () => {
+        this.counter.next(this.counterIncrement++);
+      },
+      500);
+  }
+
+  private ngOnDestroy() {
+    clearInterval(this.timer);
+  }
+}
